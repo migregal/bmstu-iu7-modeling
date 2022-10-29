7cbf302559836fd834a5b404d2831ce935c688c5
g.mironov <g.mironov@corp.mail.ru>
2022-10-29 22:40:41 +0300

    sem-02-lab-02: post production fixes

--- sem_02/lab_02/docs/inc/lst/main.py
+++ sem_02/lab_02/docs/inc/lst/main.py
@@ -1,76 +1,17 @@
 import random
 import time
 
+random.seed(time.time())
+
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import numpy as np
 import pandas as pd
 import networkx as nx
 
-random.seed(time.time())
 
-import ipywidgets as widgets
-
-states_number_input = widgets.BoundedIntText(value=0, min=0, max=100, step=1, layout=widgets.Layout(width="50px"))
-
-randomize_input = widgets.Checkbox(value=False, description='Случайное заполнение', indent=False)
-
-display(widgets.Label("Количество состояний в системе"))
-display(states_number_input)
-display(randomize_input)
-
-states_n = states_number_input.value
-
-matrix_inputs = []
-items = []
-
-items += [widgets.Label("Состояния")]
-items += [widgets.Label(f"$$\\text{{в }}S_{{{i+1}}}$$") for i in range(states_n)]
-
-for i in range(states_n):
-    items += [widgets.Label(f"$$\\text{{из }}S_{{{i+1}}}$$")]
-
-    inputs = [
-        widgets.BoundedFloatText(value= 0 if not randomize_input.value else round(random.random(), 2), min=0, max=1, step=0.01, layout=widgets.Layout(width="50px"))
-        for _ in range(states_n)
-    ]
-    matrix_inputs += [inputs]
-    items += inputs
-
-display(widgets.Label("Таблица вероятностей перехода"))
-display(widgets.GridBox(items, layout=widgets.Layout(grid_template_columns=f"repeat({states_n + 1}, 1fr)", overflow="auto")))
-
-def get_matrix():
-    return np.array([[float(matrix_inputs[i][j].value) for j in range(states_n)] for i in range(states_n)])
-
-m = get_matrix()
-
-G = nx.from_numpy_matrix(m, create_using=nx.DiGraph)
-layout = nx.circular_layout(G)
-
-nx.draw(G, layout, node_size=600)
-nx.draw_networkx_labels(G, pos=layout, labels={i: f'S_{i+1}' for i in range(states_n)}, font_color='yellow', font_size=12)
-nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=nx.get_edge_attributes(G,'weight'), label_pos=0.2)
-plt.show()
-
-t_max_input = widgets.BoundedFloatText(value=0, min=0, max=1000, step=1, description="Tmax")
-t_n_input = widgets.BoundedIntText(value=0, min=0, max=10e4, step=1, description="N")
-
-display(widgets.Label("Время моделирования"))
-display(t_max_input)
-display(t_n_input)
-
-eps = 1e-6
-dt = 0.1
-tn = t_n_input.value
-tmax = t_max_input.value
-
-m = get_matrix()
-
-m = m / m.sum(axis=1).reshape((-1, 1))
-
-def find_stable(matrix):
-    b = [0] * (len(matrix) - 1) + [1]
+def find_steady_ps(matrix: np.ndarray) -> np.ndarray:
+    b = np.array([0] * (len(matrix) - 1) + [1])
 
     matrix_to_solve = matrix.copy().transpose()
     matrix_to_solve -= np.diag(matrix.sum(axis=1))
@@ -78,53 +19,94 @@ def find_stable(matrix):
 
     return np.linalg.solve(matrix_to_solve, b)
 
-def solve_ode(matrix, start_probs, tn, tmax, steady_states):
+
+def find_steady_ts(
+    ts: np.ndarray, ps: np.ndarray, steady_ps: np.ndarray, eps: float = 1e-6
+) -> np.ndarray:
+    steady_ts = np.zeros(len(steady_ps))
+
+    for i, row in enumerate(ps):
+        for p, t in reversed(list(zip(row, ts))):
+            if abs(steady_ps[i] - p) > eps:
+                steady_ts[i] = t
+                break
+
+    return steady_ts
+
+
+def solve_ode(matrix: np.ndarray, start_probs: np.ndarray, tn: int, tmax: int):
     matrix_to_solve = matrix.copy().transpose()
     matrix_to_solve -= np.diag(matrix.sum(axis=1))
 
     ts = np.linspace(0, tmax, tn)
 
-    results = integrate.odeint(vectorfields, start_probs, ts, args=(matrix_to_solve,), atol=1.0e-8, rtol=1.0e-6).transpose()
+    ps = integrate.odeint(
+        lambda w, _: matrix_to_solve @ w, start_probs, ts, atol=1.0e-8, rtol=1.0e-6
+    ).transpose()
+    return ts, ps
 
-    steady_ts = []
 
-    for i in range(len(results)):
-        row = results[i]
-        flag = True
-        for j in range(len(row) - 1, -1, -1):
-            if abs(steady_states[i] - row[j]) > eps:
-                steady_ts.append(ts[j])
-                flag = False
-                break
-        if flag:
-            steady_ts.append(0)
+def draw_graph(matrix) -> None:
+    G = nx.from_numpy_matrix(matrix, create_using=nx.DiGraph)
+    layout = nx.circular_layout(G)
 
-    return ts, results, steady_ts
+    nx.draw(G, layout, node_size=600)
+    nx.draw_networkx_labels(
+        G,
+        pos=layout,
+        labels={i: f"$S_{i+1}$" for i in range(len(matrix))},
+        font_color="yellow",
+        font_size=12,
+    )
+    nx.draw_networkx_edge_labels(
+        G, pos=layout, edge_labels=nx.get_edge_attributes(G, "weight"), label_pos=0.2
+    )
+    plt.show()
 
-def vectorfields(w, _, matrix_to_solve):
-    f = []
-    for i in range(len(w)):
-        f.append(0)
-        for p, lambda_coeff in zip(w, matrix_to_solve[i]):
-            f[i] += p * lambda_coeff
 
-    return f
+def draw_plot(ts: np.ndarray, ps: np.ndarray) -> None:
+    for i, p in enumerate(ps):
+        plt.plot(ts, p, label=f"$P_{{{i}}}$")
+    plt.legend()
+    plt.show()
 
-def find_stable_time(matrix, start_probs, tn, tmax):
-    ps = find_stable(matrix)
-    return solve_ode(matrix, start_probs, tn, tmax, ps)
 
-stable = find_stable(m).reshape((1, -1))
+def main():
+    tn = 10**5
+    tmax = 10
 
-start_state_probs = m[0]
+    m = np.array(
+        [
+            [0, 0, 1, 0],
+            [4, 0, 3, 0],
+            [0, 3, 0, 4],
+            [0, 1, 1, 0],
+        ]
+    )
 
-ts, data, steady = find_stable_time(m, start_state_probs, tn, tmax)
+    draw_graph(m)
 
-df = pd.DataFrame.from_dict({'Стабильное состояние': find_stable(m), 'Время': steady}, orient='index', columns=[f"$$S_{{{i+1}}}$$" for i in range(states_n)])
-df.style.format("{:.3f}")
-df.to_latex(float_format="%.3f")
+    start_probs = m[0].copy()
 
-for i in range(len(data)):
-    plt.plot(ts, data[i], label=f"$P_{{{i}}}$")
-plt.legend()
-plt.show()
+    ts, ps = solve_ode(m, start_probs, tn, tmax)
+
+    draw_plot(ts, ps)
+
+    steady_ps = find_steady_ps(m)
+    steady_ts = find_steady_ts(ts, ps, steady_ps)
+
+    df = pd.DataFrame.from_dict(
+        {
+            "Вероятностная константа": steady_ps,
+            "Время достижения вероятностной константы": steady_ts,
+        },
+        orient="index",
+        columns=[f"$S_{{{i+1}}}$" for i in range(len(m))],
+    )
+
+    df.style.format("{:.3f}")
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
